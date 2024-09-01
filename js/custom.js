93d14d1b922941d41eaa0b38f0eafa0a47c38b31
pro-masud <promasudbd@gmail.com>
2024-09-01 08:01:29 +0600

    owl carousel jQuery plugin active and Work Successfully worked

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $('.item').owlCarousel({
         loop:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        animateOut: 'fadeOut',
         margin:10,
         nav:true,
         responsive:{
