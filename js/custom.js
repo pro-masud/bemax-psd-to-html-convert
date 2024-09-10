bf7aaa12a39b06ecad553775c3c4aa702437d865
pro-masud <promasudbd@gmail.com>
2024-09-10 08:22:46 +0600

    feat: project section design complete

--- js/custom.js
+++ js/custom.js
@@ -11,5 +11,31 @@ $(document).ready(function(){
                 items:1
             }
         }
+    });
+
+    $('.project-wrapper').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        animateOut: 'fadeOut',
+        nav:false,
+        dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            578:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            },
+            1200:{
+                items:4
+            },
+        }
     })
 });
